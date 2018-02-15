d46287388982a8d2476c92e03b8cac0c66185cfd
Inficare Pvt. Ltd <inficare@Inficares-Mac-mini.local>
2018-02-15 12:54:32 +0545

    initial commit

---
+++ ThreeRingControl/ThreeRingControl.h
@@ -0,0 +1,19 @@
+//
+//  ThreeRingControl.h
+//  ThreeRingControl
+//
+//  Created by Inficare Pvt. Ltd. on 15/02/2018.
+//  Copyright © 2018 Inficare Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeRingControl.
+FOUNDATION_EXPORT double ThreeRingControlVersionNumber;
+
+//! Project version string for ThreeRingControl.
+FOUNDATION_EXPORT const unsigned char ThreeRingControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeRingControl/PublicHeader.h>
+
+
